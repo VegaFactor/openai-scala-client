51a21c380fc8b43ec546777c0654d54b710827e7
Yahor Filipchyk <yahor.filipchyk@vegafactor.com>
2025-10-13 12:14:49 -0400

    Increase frame size

--- perplexity-sonar-client/src/main/scala/io/cequence/openaiscala/perplexity/service/impl/SonarServiceImpl.scala
+++ perplexity-sonar-client/src/main/scala/io/cequence/openaiscala/perplexity/service/impl/SonarServiceImpl.scala
@@ -63,7 +63,8 @@ private[service] class SonarServiceImpl(
         EndPoint.chatCompletion.toString(),
         "POST",
         bodyParams = stringParams,
-        framingDelimiter = "\r\n\r\n"
+        framingDelimiter = "\r\n\r\n",
+        maxFrameLength = Some(100_000)
       )
       .map { json =>
         (json \ "error").toOption.map { error =>
